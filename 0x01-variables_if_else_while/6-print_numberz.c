3ea16f4a66385fd487b2e65211f5dcf68b1411da
akansha9j <akansha9j@gmail.com>
2017-02-10 02:54:45 +0000

    Initial commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/* This program prints numbers from 0 to 9 with
+ * the help pf putchar function.
+ */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar((int)'0' + i);
+	}
+
+	putchar('\n');
+	return (0);
+}
