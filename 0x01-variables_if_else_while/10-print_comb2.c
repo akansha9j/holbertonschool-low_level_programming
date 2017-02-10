934307dc070b14a7f47d54f0c0b211bdad633b03
akansha9j <akansha9j@gmail.com>
2017-02-10 05:28:34 +0000

    Initial commit

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/* This program prints the characters from 00
+ * to 99 with the help of putchar.
+ */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <=9; j++)
+		{
+			putchar('0' + i);
+			putchar('0' + j);
+			if (!(i == 9 && j == 9))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
