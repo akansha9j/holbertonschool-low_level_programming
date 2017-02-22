df49892ef3588588d048fe9d95851a052593ab84
akansha9j <akansha9j@gmail.com>
2017-02-22 05:34:08 +0000

    Updated file

--- 0x04-pointers_arrays_strings/8-print_array.c
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -15,13 +15,16 @@ void print_array(int *a, int n)
 
 	for (i = 0; i <= (n - 1); i++)
 	{
-		if (i == (n - 1))
+		if (a[i] != '\0')
 		{
-			printf("%d\n", a[i]);
-		}
-		else
-		{
-			printf("%d, ", a[i]);
+			if (i == (n - 1))
+			{
+				printf("%d\n", a[i]);
+			}
+			else
+			{
+				printf("%d, ", a[i]);
+			}
 		}
 	}
 }
