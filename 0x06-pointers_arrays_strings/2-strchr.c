9717d72872c5a3e552ae755d22eed3b8cc48e20f
akansha9j <akansha9j@gmail.com>
2017-02-27 22:08:18 +0000

    #2

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+/**
+ * *_strchr - locates a character in a string.
+ * @s: pointer to the input string
+ * @c: character to be located
+ *
+ * Return: pointer to the first occurrence of the character
+ * NULL, if the character is not found
+ *
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+	char *ch;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			ch = &s[i];
+			return (ch);
+		}
+	}
+	return ('\0');
+}
