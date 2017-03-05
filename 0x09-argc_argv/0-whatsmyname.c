99b2a9363f828c66348cec94f56e0c695845ba91
akansha9j <akansha9j@gmail.com>
2017-03-05 19:59:51 +0000

    Initial commit

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the program name followed by a new line
+ * @argc: count of arguments
+ * @argv: array of pointers
+ *
+ * Return: 0
+ *
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
